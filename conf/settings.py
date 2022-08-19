fad82c213a143192fcb624aadfdd0453bc0efc97
임태규 <kyu5787@naver.com>
2022-08-19 23:13:15 +0900

    mm

--- conf/settings.py
+++ conf/settings.py
@@ -41,7 +41,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".cloudtype.app"]
+CSRF_TRUSTED_ORIGINS = ['https://*.cloudtype.app']
 
 
 # Application definition
